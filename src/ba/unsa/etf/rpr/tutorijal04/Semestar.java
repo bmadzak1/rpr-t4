82d422afe22f5aeae0536b2c86eef1ea09c121ce
bmadzak1 <bmadzak1@etf.unsa.ba>
2018-11-10 23:20:27 +0100

    Semestar klasa

--- src/ba/unsa/etf/rpr/tutorijal04/Semestar.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Semestar.java
@@ -9,22 +9,22 @@ public class Semestar {
     ArrayList<Predmet> izborniPredmeti = new ArrayList<Predmet>();
 
     public Semestar(int broj){
-
+        brojSemestra = broj;
     }
 
     public void dodajObavezniPredmet(Predmet predmet){
-
+        obavezniPredmeti.add(predmet);
     }
 
     public void dodajIzborniPredmet(Predmet predmet){
-
+        izborniPredmeti.add(predmet);
     }
 
     public ArrayList<Predmet> dajSveObaveznePredmete(){
-        return null;
+        return obavezniPredmeti;
     }
 
     public ArrayList<Predmet> dajSveIzbornePredmete(){
-        return null;
+        return izborniPredmeti;
     }
 }
